c5d9e4a50acbc275374b32b0688fd0db74acc2f9
Shivraj Vinayak Darekar <darekar1005@gmail.com>
2025-06-23 14:38:27 +0530

    Solve production isue

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,32 +1,33 @@
-const express=require('express');
-const { registerUser , loginUser , logout} = require('../controllers/authController');
+const express = require('express');
+const { registerUser, loginUser, logout } = require('../controllers/authController');
 const passport = require('passport');
 const { generateToken } = require('../utils/generateToken');
-const router=express.Router();
+const router = express.Router();
 
-router.post('/register',registerUser);
-router.post('/login',loginUser);
-router.get('/logout',logout);
+
+
+router.post('/register',registerUser)
+router.post('/login',loginUser)
+router.get('/logout', logout)
+
+
 
 //google oauth routes
 router.get('/google',passport.authenticate('google',{
-    scope:['profile','email']
+    scope: ['profile', 'email ']
 }))
 
 //google callback routes
-router.get('/google/callback',passport.authenticate('google',{failureRedirect: `${process.env.FRONTEND_URL}/user-login`,session:false}),
- (req,res)=>{
-
-     const accessToken = generateToken(req?.user);
-        res.cookie("auth_token", accessToken, {
-          httpOnly: true,
-          secure: true,      // required for HTTPS
-  sameSite: "none",  // required for cross-site cookies
-        });
-
-        res.redirect(`${process.env.FRONTEND_URL}`);
-    
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: `${process.env.FRONTEND_URL}/user-login`, session:false}),
+ (req,res) =>{ 
+    const accessToken = generateToken(req?.user);
+    res.cookie("auth_token",accessToken,{
+        httpOnly: true,
+        sameSite:"none",
+        secure:true
+    })
+   res.redirect(`${process.env.FRONTEND_URL}`)
  }
 )
 
-module.exports=router;
+module.exports = router;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,8 +1,7 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
-const generateToken = (user) =>{
-    return jwt.sign({userId:user?._id , email : user.email},process.env.JWT_SECRET , {expiresIn:'90d'} );
-
+const generateToken = (user)=>{
+    return jwt.sign({userId: user?._id, email: user.email}, process.env.JWT_SECRET, {expiresIn: "90d"});
 }
 
-module.exports={generateToken};
+module.exports = {generateToken}
